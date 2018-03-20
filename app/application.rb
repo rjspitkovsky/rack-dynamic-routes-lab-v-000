f74f966dc87e170e0bd7b3be30d43a2ee41db3b3
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-20 18:05:59 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,22 +6,26 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path == "/items"
-
-      item_name = req.path.split("/items/").last
-      item = @@items.find{|i| i.name = item_name}
-
-      if @@items.include?(item)
-        resp.write item.price
-      else
-        resp.status = 400
-        resp.write "We don't have that item"
-      end
-
-    else
+    if req.path != "/items"
       resp.write "Route not found"
       resp.status = 404
-    end
+    end 
+
+    #
+    #   item_name = req.path.split("/items/").last
+    #   item = @@items.find{|i| i.name = item_name}
+    #
+    #   if @@items.include?(item)
+    #     resp.write item.price
+    #   else
+    #     resp.status = 400
+    #     resp.write "We don't have that item"
+    #   end
+    #
+    # else
+    #   resp.write "Route not found"
+    #   resp.status = 404
+    # end
 
   end
 end
