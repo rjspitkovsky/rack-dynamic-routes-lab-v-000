cf2a2979e72a4505f5c899e8389a2988e6e5572d
Richard Spitkovsky <rjspitkovsky@gmail.com>
2018-03-20 17:59:29 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path=="/items/"
       item_name = req.path.split("/items/").last
       item = @@items.find{|i| i.name = item_name}
 
